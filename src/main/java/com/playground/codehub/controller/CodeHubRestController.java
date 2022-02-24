98b2b6cdc60c955041b29bedf0e272c3f70e5fb1
Harisha V S <harisha86@yahoo.co.uk>
2022-02-24 10:58:08 +0530

    new endpoints
    added end points.

--- src/main/java/com/playground/codehub/controller/CodeHubRestController.java
+++ src/main/java/com/playground/codehub/controller/CodeHubRestController.java
@@ -1,12 +1,30 @@
 package com.playground.codehub.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CodeHubRestController {
     @GetMapping("/")
 	public String Welcome() {
-		return "Welcome to home";
+		return "Welcome to home - Harisha";
+	}
+
+	@GetMapping("/list")
+	public List getList() {
+		List getAll = new ArrayList<>();
+		getAll.add("one");
+		getAll.add("two");
+		getAll.add("one");
+		return getAll;
+	}
+
+	@GetMapping("/{name}")
+	public String dynamicWelcome(@PathVariable String name) {
+		return name+", "+"Welcome to home";
 	}
 }
